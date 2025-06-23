bd4b7481b64598149ee71861fe7bb15fcd3eb96e
shreyaagrawal281994 <shreya.agrawal281994@gmail.com>
2025-06-23 18:06:53 +0400

    First commit

---
+++ src/test/java/com/unwind/app/UnwindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unwind.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnwindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
